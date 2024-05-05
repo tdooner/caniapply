e27e8e3e372b7f4db62129827235fffbee7c1932
Tom Dooner <tomdooner@gmail.com>
2024-05-04 22:38:37 -0700

    Initial proof of concept
    * Shows successful and unsuccessful count for each site!
    
    TODO:
    * Styling - make it all look like a cool progress bar or something
    * I think this will really pop if I can get actual selenium browser
      screenshots of each downtime.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,50 @@
 import { PrismaClient } from "@prisma/client";
+import format from "date-format";
 
 const prisma = new PrismaClient();
 
+type LastDayUptime = {
+  [key: number]: {
+    successfulCount: number,
+    unsuccessfulCount: number
+  }
+}
+const getLastDayUptimeByHour = async function(systemIds: number[]) : Promise<LastDayUptime> {
+  const uptimeBySystem : LastDayUptime = {};
+  const twentyFourHoursAgo = format("yyyy-MM-dd hh:mm:ss.SSS", new Date(new Date().getTime() - 1000*60*60*24));
+  const pings = await prisma.pings.findMany({
+    where: {
+      system_id: {
+        in: systemIds
+      },
+      created_at: {
+        gt: twentyFourHoursAgo
+      }
+    }
+  });
+  systemIds.forEach(systemId => {
+    const systemPings = pings.filter(ping => ping.system_id === systemId);
+    uptimeBySystem[systemId] = {
+      successfulCount: systemPings.reduce((count, ping) => ping.http_status < 300 ? count + 1 : count, 0),
+      unsuccessfulCount: systemPings.reduce((count, ping) => ping.http_status > 300 ? count + 1 : count, 0),
+    };
+  });
+
+  return uptimeBySystem;
+}
+
 export default async function Home() {
-  const results = await prisma.pings.findMany({ take: 100 });
+  const systems = await prisma.systems.findMany();
+  const uptimeBySystem = await getLastDayUptimeByHour(systems.map(system => system.id));
 
   return (
     <main>
-      {JSON.stringify(results)}
+      {systems.map(system => {
+        return <>
+          <div>{system.name}</div>
+          <div>{JSON.stringify(uptimeBySystem[system.id])}</div>
+        </>
+      })}
     </main>
   );
 }
