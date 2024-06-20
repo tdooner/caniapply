228a4aebaa1669634c16c432940aa587edf89beb
Tom Dooner <tomdooner@gmail.com>
2024-06-19 20:16:52 -0700

    Rewrite extractLoadResult to also account for load failures
    Better logging and catching load failures (TLS issues, etc.) that would
    prevent a "finishedLoadingEvent" from triggering.

--- scraper/scrape.ts
+++ scraper/scrape.ts
@@ -12,6 +12,12 @@ const FREE_MEMORY_BYTES = os.freemem()
 const NUM_BROWSERS = process.env.NUM_BROWSERS ?
   Number.parseInt(process.env.NUM_BROWSERS) :
   Math.min(8, Math.ceil(FREE_MEMORY_BYTES / BROWSER_MEM_USAGE_BYTES))
+const HTTPStatus = {
+  UNKNOWN: 0,
+  TLS_CERT_COMMON_NAME_INVALID: 700,
+  OTHER_ERROR: 990,
+  EXCEPTION: 999,
+}
 
 const initializeBrowser = async function(): Promise<WebDriver> {
   console.log("Starting browser...")
@@ -44,22 +50,56 @@ type PageLoadResult = {
   loadTimeSeconds: number | undefined,
   httpStatus: number | undefined
 }
-const extractLoadResult = function(entries : logging.Entry[]) : PageLoadResult {
-  let loadTimeSeconds, httpStatus
-  const parsedEntries = entries.map(entry => JSON.parse(entry.message))
-  const startedLoadingEvent = parsedEntries.find(entry => entry["message"]["method"] === "Network.requestWillBeSent")
-  const finishedLoadingEvent = parsedEntries.find(entry => entry["message"]["method"] === "Page.loadEventFired")
-  const responseEvent = parsedEntries.filter(entry => entry["message"]["params"]["requestId"] === startedLoadingEvent["message"]["params"]["requestId"] &&
-                                               entry["message"]["method"] === "Network.responseReceived")
-  
-  if (responseEvent.length > 1) {
-    console.warn("More than one responseEvent!", responseEvent)
-  } else {
-    httpStatus = responseEvent[0]["message"]["params"]["response"]["status"]
+type ParsedEntry = {
+  message: {
+    method: string
+    params: {
+      timestamp: number
+      requestId: number | string
+      errorText: string
+      response: {
+        status: number
+      }
+    }
+  }
+}
+const diffTimestamps = function(entry1 : ParsedEntry | undefined, entry2 : ParsedEntry | undefined) : number | undefined{
+  if (!entry1 || !entry2) {
+    return
   }
 
-  if (startedLoadingEvent && finishedLoadingEvent) {
-     loadTimeSeconds = finishedLoadingEvent["message"]["params"]["timestamp"] - startedLoadingEvent["message"]["params"]["timestamp"]
+  return entry2.message.params.timestamp - entry1.message.params.timestamp
+}
+
+const extractLoadResult = function(entries : logging.Entry[]) : PageLoadResult {
+  let loadTimeSeconds, httpStatus
+  const parsedEntries = entries.map(entry => JSON.parse(entry.message)) as ParsedEntry[]
+
+  const startedLoadingEvent = parsedEntries.find(entry => entry.message.method === "Network.requestWillBeSent")
+  const finishedLoadingEvent = parsedEntries.find(entry => entry.message.method === "Page.loadEventFired")
+  const failedLoadingEvent = parsedEntries.find(entry => entry.message.method === "Network.loadingFailed" &&
+                                                entry.message.params.requestId === startedLoadingEvent?.message.params.requestId)
+  const responseEvent = parsedEntries.filter(entry => entry.message.method === "Network.responseReceived" &&
+                                             entry.message.params.requestId === startedLoadingEvent?.message.params.requestId)
+
+  if (failedLoadingEvent) {
+    loadTimeSeconds = diffTimestamps(startedLoadingEvent, failedLoadingEvent)
+    const errorText = failedLoadingEvent.message.params.errorText
+    console.error("Failed to load: ", errorText)
+    if (errorText === "net::ERR_CERT_COMMON_NAME_INVALID") {
+      httpStatus = HTTPStatus.TLS_CERT_COMMON_NAME_INVALID
+    } else {
+      httpStatus = HTTPStatus.OTHER_ERROR
+    }
+  } else if (responseEvent.length > 1) {
+    console.error("More than one responseEvent!", responseEvent)
+  } else if (responseEvent.length === 0) {
+    console.error("No responseEvent!")
+  } else if (startedLoadingEvent && finishedLoadingEvent) {
+    loadTimeSeconds = diffTimestamps(startedLoadingEvent, finishedLoadingEvent)
+    httpStatus = responseEvent[0].message.params.response.status
+  } else {
+    console.error("No started & finished loading events!", startedLoadingEvent, finishedLoadingEvent)
   }
 
   return { httpStatus, loadTimeSeconds }
@@ -100,7 +140,7 @@ export const scrapeAll = async function() {
           data: {
             system_id: system.id,
             latency: loadResult.loadTimeSeconds,
-            http_status: loadResult.httpStatus || 0,
+            http_status: loadResult.httpStatus || HTTPStatus.UNKNOWN,
             body_length: response.length,
             created_at: format(DATABASE_DATE_FORMAT, new Date())
           }
@@ -114,7 +154,7 @@ export const scrapeAll = async function() {
         data: {
           system_id: system.id,
           latency: 0,
-          http_status: 999,
+          http_status: HTTPStatus.EXCEPTION,
           body_length: 0,
           created_at: format(DATABASE_DATE_FORMAT, new Date())
         }
