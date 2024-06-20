1d998e39b62232aa79058ce814f32e011623e161
Tom Dooner <tomdooner@gmail.com>
2024-06-19 21:23:12 -0700

    Tweak the YAML importer logic a bit
    * Delete any systems not present after successfully importing the YAML
    * Update a couple URLs to point to the actual systems (not the welcome
      pages)

--- scraper/import.ts
+++ scraper/import.ts
@@ -19,33 +19,50 @@ const toSlug = function(item : YamlItem) : string {
 
 export const importSystemsYaml = async function() : Promise<void> {
   console.log("Parsing ", SYSTEMS_YAML_PATH)
-  const prisma = new PrismaClient()
+  const prisma = new PrismaClient({ log: [{ emit: 'stdout', level: 'query' }] })
   const parsed = YAML.parse(fs.readFileSync(SYSTEMS_YAML_PATH).toString()) as YamlItem[]
-  parsed.forEach(async item => {
+  const upsertedIds = [] as number[]
+  let allSucceeded = true
+
+  await Promise.all(parsed.map(async item => {
     const slug = toSlug(item)
-    console.log(item, toSlug(item))
-    const system = await prisma.systems.upsert({
-      create: {
-        slug,
-        host: item.url,
-        uri: item.url,
-        jurisdiction: item.jurisdiction,
-        programs: item.programs,
-        name: item.name,
-      },
-      update: {
-        host: item.url,
-        uri: item.url,
-        jurisdiction: item.jurisdiction,
-        programs: item.programs,
-        name: item.name,
-      },
+    try {
+      const system = await prisma.systems.upsert({
+        create: {
+          slug,
+          host: item.url,
+          uri: item.url,
+          jurisdiction: item.jurisdiction,
+          programs: item.programs,
+          name: item.name,
+        },
+        update: {
+          host: item.url,
+          uri: item.url,
+          jurisdiction: item.jurisdiction,
+          programs: item.programs,
+          name: item.name,
+        },
+        where: { slug }
+      })
+      upsertedIds.push(system.id)
+      console.log("saved: ", system.name, "(", system.slug, ")")
+    } catch (ex) {
+      allSucceeded = false
+      console.error("could not save: ", item.name, "error:", ex)
+    }
+  }))
+
+  if (allSucceeded) {
+    const deletedSystems = await prisma.systems.deleteMany({
       where: {
-        slug: slug,
+        id: {
+          notIn: upsertedIds
+        }
       }
     })
-    console.log(system)
-  })
+    console.log("deleted", deletedSystems.count, "systems")
+  }
 }
 
 
